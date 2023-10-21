78327b97a52c8d25050258370f07378ea5f1b6f7
Enma Ormaza <enma.ormaza@mataro.epiaedu.cat>
2023-10-21 13:49:13 +0200

    COMPLETE EX-02.js

--- js/exercise-02.js
+++ js/exercise-02.js
@@ -5,3 +5,12 @@
     
     Repeat the above steps but try using a const variable. Comment this new part of code and explain what is happening.
 */
+let varExample;
+varExample = "Moo!";
+console.log(varExample);
+
+const varExample2; /*you can't declare a const variable without initializing it.
+                    constant variable does not have undefined value, therefore if it's not inizialized it will break*/
+console.log(varExample2);
+
+varExample2 = "Moo!"; 
