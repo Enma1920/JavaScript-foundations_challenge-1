1da41cf6c0b2a211831f6902941572850e36794b
Enma Ormaza <enma.ormaza@mataro.epiaedu.cat>
2023-10-21 12:22:56 +0200

    COMPLETE EX-0.js

--- js/exercise-00.js
+++ js/exercise-00.js
@@ -8,3 +8,14 @@
 
    Log out each variable to the terminal at the end.
 */
+const constantString = "Hello World";
+const constantNumber = 10;
+const constantBoolean = true;
+const constantNull = null;
+const constantUndefined = undefined;
+
+console.log("String:", constantString);
+console.log("Number:", constantNumber);
+console.log("Boolean:", constantBoolean );
+console.log("Null:", constantNull);
+console.log("Undefined:", constantUndefined);
